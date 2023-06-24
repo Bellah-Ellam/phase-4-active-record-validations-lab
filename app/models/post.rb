0fe8016ee75ce6b4b81c56cf8ee4156a24eaefb2
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-06-24 10:46:55 +0300

    complete lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :sufficiently_clickbait_y
+  
+    private
+  
+    def sufficiently_clickbait_y
+      return if title.blank?
+  
+      clickbait_keywords = ['Won\'t Believe', 'Secret', 'Top', 'Guess']
+      unless clickbait_keywords.any? { |keyword| title.include?(keyword) }
+        errors.add(:title, 'should be sufficiently clickbait-y')
+      end
+    end
+  end
+  
